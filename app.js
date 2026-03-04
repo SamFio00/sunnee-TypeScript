cb5f97b3343c81b81f0ba9013f0f04b2f19b8a96
SamFio00 <samuele.fiorini2000fr@gmail.com>
2026-03-04 11:42:56 +0100

    added comments

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
+// Implementazione della classe Prodotto
 var Prodotto = /** @class */ (function () {
     function Prodotto(tipo, taglia, colore, stato, id) {
         this.tipo = tipo;
@@ -8,12 +9,18 @@ var Prodotto = /** @class */ (function () {
         this.stato = stato;
         this.id = id;
     }
+    // Metodo per assegnare un cliente al prodotto
     Prodotto.prototype.assegnaCliente = function (cliente) {
-        this.stato = "ordinato";
+        if (this.stato !== "disponibile") {
+            console.log("Il prodotto ".concat(this.id, " non \u00E8 disponibile per l'assegnazione al cliente ").concat(cliente.nome, " ").concat(cliente.cognome, "."));
+            return;
+        }
+        this.stato = "ordinato"; // Cambia lo stato del prodotto a "ordinato" quando viene assegnato a un cliente
         console.log("Il prodotto ".concat(this.id, " \u00E8 stato assegnato al cliente ").concat(cliente.nome, " ").concat(cliente.cognome, "."));
     };
     return Prodotto;
 }());
+// Implementazione della classe Cliente
 var Cliente = /** @class */ (function () {
     function Cliente(nome, cognome, email, metodoPagamento) {
         this.nome = nome;
@@ -32,6 +39,7 @@ var Cliente = /** @class */ (function () {
     };
     return Cliente;
 }());
+// Implementazione della classe ProcessoProduzione
 var ProcessoProduzione = /** @class */ (function () {
     function ProcessoProduzione(nomeProcesso, descrizione, prodottiInProduzione) {
         if (prodottiInProduzione === void 0) { prodottiInProduzione = []; }
@@ -40,13 +48,21 @@ var ProcessoProduzione = /** @class */ (function () {
         this.prodottiInProduzione = prodottiInProduzione;
     }
     ProcessoProduzione.prototype.aggiungiProdotto = function (prodotto) {
-        this.prodottiInProduzione.push(prodotto);
+        this.prodottiInProduzione.push(prodotto); // Aggiunge il prodotto all'array dei prodotti in produzione
         console.log("Il prodotto ".concat(prodotto.id, " \u00E8 stato aggiunto al processo di produzione ").concat(this.nomeProcesso, "."));
     };
     return ProcessoProduzione;
 }());
+// Esempio di utilizzo delle classi e interfacce
 var prodotto1 = new Prodotto("costume da bagno", "M", "rosso", "disponibile", "SUN001");
 var prodotto2 = new Prodotto("pareo", "L", "blu", "disponibile", "SUN002");
 var prodotto3 = new Prodotto("cappello", "S", "giallo", "disponibile", "SUN003");
 var cliente1 = new Cliente("Mario", "Rossi", "Mrossi@gmail.com", "carta");
 var cliente2 = new Cliente("Gino", "Panevino", "gpane@gmail.com", "bonifico");
+var processo1 = new ProcessoProduzione("Riciclo plastica marina", "Trasformazione di reti da pesca dismesse in filato tessile sostenibile per la produzione di costumi da bagno e accessori.");
+processo1.aggiungiProdotto(prodotto1); // Il prodotto SUN001 è stato aggiunto al processo di produzione Riciclo plastica marina.
+processo1.aggiungiProdotto(prodotto2); // Il prodotto SUN002 è stato aggiunto al processo di produzione Riciclo plastica marina.
+processo1.aggiungiProdotto(prodotto3); // Il prodotto SUN003 è stato aggiunto al processo di produzione Riciclo plastica marina.
+cliente1.ordinaProdotto(prodotto1); // Prodotto ordinato con successo da cliente1
+cliente2.ordinaProdotto(prodotto2); // Prodotto ordinato con successo da cliente2
+cliente2.ordinaProdotto(prodotto1); // Prodotto già ordinato da cliente1, non disponibile per cliente2
--- app.ts
+++ app.ts
@@ -1,7 +1,9 @@
+// Tipi per il prodotto, cliente e processo di produzione
 type TipoProdotto = "costume da bagno" | "pareo" | "cappello";
 type StatoProdotto = "disponibile" | "esaurito" | "ordinato";
 type TagliaProdotto = "XS" | "S" | "M" | "L" | "XL" | "XXL";
 
+// Interfaccia per il prodotto
 interface IProdotto {
     tipo: TipoProdotto;
     taglia: TagliaProdotto;
@@ -14,15 +16,17 @@ interface IProdotto {
 
 type MetodoPagamento = "carta" | "paypal" | "bonifico";
 
+// Interfaccia per il cliente
 interface ICliente {
     nome: string;
     cognome: string;
     email: string;
     metodoPagamento: MetodoPagamento;
 
-    ordinaProdotto(prodotto: IProdotto):void;
+    ordinaProdotto(prodotto: IProdotto): void;
 }
 
+// Interfaccia per il processo di produzione
 interface IProcessoProduzione {
     nomeProcesso: string;
     descrizione: string;
@@ -31,6 +35,7 @@ interface IProcessoProduzione {
     aggiungiProdotto(prodotto: IProdotto): void;
 }
 
+// Implementazione della classe Prodotto
 class Prodotto implements IProdotto {
     constructor(
         public tipo: TipoProdotto,
@@ -40,12 +45,21 @@ class Prodotto implements IProdotto {
         public id: string
     ) {}
 
+    // Metodo per assegnare un cliente al prodotto
     assegnaCliente(cliente: ICliente): void {
-        this.stato = "ordinato";
+        if (this.stato !== "disponibile") {
+            console.log(
+                `Il prodotto ${this.id} non è disponibile per l'assegnazione al cliente ${cliente.nome} ${cliente.cognome}.`
+            );
+            return;
+        }
+
+        this.stato = "ordinato"; // Cambia lo stato del prodotto a "ordinato" quando viene assegnato a un cliente
         console.log(`Il prodotto ${this.id} è stato assegnato al cliente ${cliente.nome} ${cliente.cognome}.`);
     }
 }
 
+// Implementazione della classe Cliente
 class Cliente implements ICliente {
     constructor(
         public nome: string,
@@ -54,16 +68,17 @@ class Cliente implements ICliente {
         public metodoPagamento: MetodoPagamento
     ) {}
 
-    ordinaProdotto(prodotto: IProdotto): void {
+    ordinaProdotto(prodotto: IProdotto): void { // Verifica se il prodotto è disponibile prima di ordinarlo
         if (prodotto.stato === "disponibile") {
             prodotto.assegnaCliente(this);
-            console.log(`Il cliente ${this.nome} ${this.cognome} ha ordinato il prodotto ${prodotto.id}.`); 
+            console.log(`Il cliente ${this.nome} ${this.cognome} ha ordinato il prodotto ${prodotto.id}.`);
         } else {
             console.log(`Il prodotto ${prodotto.id} non è disponibile per l'ordine.`);
         }
-        }
+    }
 }
 
+// Implementazione della classe ProcessoProduzione
 class ProcessoProduzione implements IProcessoProduzione {
     constructor(
         public nomeProcesso: string,
@@ -72,26 +87,59 @@ class ProcessoProduzione implements IProcessoProduzione {
     ) {}
 
     aggiungiProdotto(prodotto: IProdotto): void {
-        this.prodottiInProduzione.push(prodotto);
+        this.prodottiInProduzione.push(prodotto); // Aggiunge il prodotto all'array dei prodotti in produzione
         console.log(`Il prodotto ${prodotto.id} è stato aggiunto al processo di produzione ${this.nomeProcesso}.`);
     }
-
 }
 
-const prodotto1 = new Prodotto("costume da bagno", "M", "rosso", "disponibile", "SUN001");
-const prodotto2 = new Prodotto("pareo", "L", "blu", "disponibile", "SUN002");
-const prodotto3 = new Prodotto("cappello", "S", "giallo", "disponibile", "SUN003");
+// Esempio di utilizzo delle classi e interfacce
+const prodotto1 = new Prodotto(
+    "costume da bagno", 
+    "M", 
+    "rosso", 
+    "disponibile", 
+    "SUN001"
+);
+const prodotto2 = new Prodotto(
+    "pareo", 
+    "L", 
+    "blu", 
+    "disponibile", 
+    "SUN002"
+);
+const prodotto3 = new Prodotto(
+    "cappello", 
+    "S", 
+    "giallo", 
+    "disponibile", 
+    "SUN003"
+);
 
-const cliente1 = new Cliente("Mario", "Rossi", "Mrossi@gmail.com", "carta");
-const cliente2 = new Cliente("Gino", "Panevino", "gpane@gmail.com", "bonifico");
 
-const processo1 = new ProcessoProduzione("Riciclo plastica marina", "Trasformazione di reti da pesca dismesse in filato tessile sostenibile per la produzione di costumi da bagno e accessori.");
+const cliente1 = new Cliente(
+    "Mario", 
+    "Rossi", 
+    "Mrossi@gmail.com", 
+    "carta"
+);
+const cliente2 = new Cliente(
+    "Gino", 
+    "Panevino", 
+    "gpane@gmail.com", 
+    "bonifico"
+);
 
-processo1.aggiungiProdotto(prodotto1);
-processo1.aggiungiProdotto(prodotto2);
-processo1.aggiungiProdotto(prodotto3);
+const processo1 = new ProcessoProduzione(
+    "Riciclo plastica marina",
+    "Trasformazione di reti da pesca dismesse in filato tessile sostenibile per la produzione di costumi da bagno e accessori."
+);
 
-cliente1.ordinaProdotto(prodotto1);
-cliente2.ordinaProdotto(prodotto2);
+processo1.aggiungiProdotto(prodotto1); // Il prodotto SUN001 è stato aggiunto al processo di produzione Riciclo plastica marina.
+processo1.aggiungiProdotto(prodotto2); // Il prodotto SUN002 è stato aggiunto al processo di produzione Riciclo plastica marina.
+processo1.aggiungiProdotto(prodotto3); // Il prodotto SUN003 è stato aggiunto al processo di produzione Riciclo plastica marina.
 
-export{}
+cliente1.ordinaProdotto(prodotto1); // Prodotto ordinato con successo da cliente1
+cliente2.ordinaProdotto(prodotto2); // Prodotto ordinato con successo da cliente2
+cliente2.ordinaProdotto(prodotto1); // Prodotto già ordinato da cliente1, non disponibile per cliente2
+
+export {};
